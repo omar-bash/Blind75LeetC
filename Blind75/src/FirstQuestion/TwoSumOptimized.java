51c15371139d4b31e5e583039508f542d5998ae4
omar-bash <o.hussein33@gmail.com>
2022-03-17 17:47:25 -0600

    Optimized Solution o(n)

---
+++ Blind75/src/FirstQuestion/TwoSumOptimized.java
@@ -0,0 +1,53 @@
+package FirstQuestion;
+
+import java.util.HashMap;
+
+public class TwoSumOptimized {
+	public static int[] twoSum(int[] nums, int target) {
+        int [] arr= {-1,-1};
+        int firstNum=0;
+        int secondNum=0;
+        HashMap <Integer,Integer> answer= new HashMap<Integer,Integer> ();
+        for(int i=0;i<nums.length;i++) {
+        	firstNum=nums[i];
+        	secondNum=target-firstNum;
+        	if(answer.containsKey(secondNum)) {
+        		arr[0]=i;
+        		arr[1]=answer.get(secondNum);
+        	}
+        	answer.put(nums[i],i);
+        	
+        }
+        return arr;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] nums1={2,7,11,15};
+		int [] nums2= {3,2,4};
+		int [] nums3= {3,3};
+		int target=9;
+		int target2=6;
+		int [] ans1= new int [2];
+		int [] ans2= new int [2];
+		int [] ans3= new int [2];
+		ans1=twoSum(nums1,target);
+		ans2=twoSum(nums2,target2);
+		ans3=twoSum(nums3,target2);
+		System.out.println("First Output");
+		System.out.println("--------------");
+		for(int i=0;i<ans1.length;i++) {
+			System.out.println(ans1[i]);
+		}
+		System.out.println("Second Output");
+		System.out.println("-----------------------");
+		for(int i=0;i<ans2.length;i++) {
+			System.out.println(ans2[i]);
+		}
+		System.out.println("Third Output");
+		System.out.println("-----------------");
+		for(int i=0;i<ans3.length;i++) {
+			System.out.println(ans3[i]);
+		}
+	}
+
+}
